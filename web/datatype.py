dfcfef26f0d07361134db774087c358f98869694
lawrence <lawrence18880816@gmail.com>
2025-06-14 20:45:51 +0800

    33

---
+++ web/datatype.py
@@ -0,0 +1,24 @@
+#數值
+3.5
+
+#字串
+"hello world"
+
+#布林值
+True
+False
+
+#有順序、可變動的列表LIST
+[3,4,5,6]
+["HELLO","WORLD"]
+
+#有順序、不可變動的列表TUPLE
+(3,4,5,69)
+("HELLO","WORLD")
+
+#集合 SET:沒有順序
+{3,4,5,69}
+{"HELLO","WORLD"}
+
+#字典 dictionary
+{"apple":"蘋果","Banana":"香蕉"}
